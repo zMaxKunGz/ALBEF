b87db244d2afb838cc7b26bc8f53376fdfffe772
Pasit Tiwawongrut <maxkung55@gmail.com>
2025-01-16 07:24:26 +0000

    Adjusted folder

--- pretrain_script.sh
+++
@@ -1,1 +0,0 @@
-torchrun --nproc_per_node=4 Pretrain.py --config ./configs/Pretrain.yaml --output_dir output/Pretrain --pos all 
---
+++ script/eval_retrieval.sh
@@ -0,0 +1,2 @@
+torchrun --nproc_per_node=2 Pretrain.py --config ./configs/Pretrain.yaml --output_dir output/Pretrain --pos NOUN --checkpoint ./pretrain-weight/ALBEF_4M.pth
+
