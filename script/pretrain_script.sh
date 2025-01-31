ea568d899c8fcda9aa391a11f936d0e0fc97928d
Pasit Tiwawongrut <maxkung55@gmail.com>
2025-01-31 11:54:48 +0000

    Modified folder

--- script/pretrain_script.sh
+++ script/pretrain_script.sh
@@ -1,2 +1,2 @@
-torchrun --nproc_per_node=2 ../Pretrain.py --config ../configs/Pretrain.yaml --output_dir ../output/Pretrain --pos NOUN
+torchrun --nproc_per_node=4 ../Pretrain.py --config ../configs/Pretrain.yaml --output_dir ../output/Pretrain --pos all
 # --checkpoint ./pretrain-weight/ALBEF_4M.pth
