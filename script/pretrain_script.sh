4c065cfa9239fb085049d61f597a52a109f3f3da
Pasit Tiwawongrut <maxkung55@gmail.com>
2025-02-02 09:17:01 +0000

    Modiefied pretrain to use modified model

--- script/pretrain_script.sh
+++ script/pretrain_script.sh
@@ -1,2 +1,2 @@
-torchrun --nproc_per_node=4 ../Pretrain.py --config ../configs/Pretrain.yaml --output_dir ../output/Pretrain --pos all
+torchrun --nproc_per_node=4 ../Pretrain.py --config ../configs/Pretrain.yaml --output_dir ../output/Pretrain --pos NOUN
 # --checkpoint ./pretrain-weight/ALBEF_4M.pth
