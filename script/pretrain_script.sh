232413d88b0c3c4ebe8ec1ff50158325cf35792b
Pasit Tiwawongrut <maxkung55@gmail.com>
2025-06-24 07:52:32 +0000

    Renamed scipt file

--- script/pretrain_script.sh
+++ script/pretrain_script.sh
@@ -1,1 +1,4 @@
-torchrun --nnodes=1 --nproc_per_node=4 ../Pretrain.py --config ../configs/Pretrain.yaml --output_dir ../output/Noqueue-42 --pos all
+torchrun --nnodes=1 --nproc_per_node=4 ../Pretrain.py \
+    --config ../configs/Pretrain.yaml \
+    --output_dir ../output/Noqueue-42 \
+    --pos all
